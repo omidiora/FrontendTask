2483654a956d1b64eb180142610ff0d701087fbf
omidiora <omidioraemmanuel@gmail.com>
2021-06-25 17:15:08 +0100

    inital commit

---
+++ backend/server.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+const express = require("express");
+const productRoutes = require("./routes/productRoutes");
+const connectDB = require("./config/db");
+const path = require('path');
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+
+// app.use("/", productRoutes);
+
+// app.use(express.static(path.join(__dirname, 'frontend/build')));
+
+
+// app.get('/*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
+// });
+
+
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "build")));
+  app.get("*", (request, response) => {
+    response.sendFile(path.join(__dirname, "build", "index.html"));
+  });
+}
+app.use("/api/products", productRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
