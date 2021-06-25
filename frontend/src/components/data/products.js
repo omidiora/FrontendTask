3f88cdc4abd590a5be32b8f5567598ef983609c2
omidiora <omidioraemmanuel@gmail.com>
2021-06-25 17:26:28 +0100

    fix add

--- frontend/src/components/data/products.js
+++ frontend/src/components/data/products.js
@@ -1,5 +1,6 @@
 const products = [
   {
+    id:1,
     name: "PlayStation 5",
     imageUrl:
       "https://images.unsplash.com/photo-1606813907291-d86efa9b94db?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80",
@@ -9,6 +10,7 @@ const products = [
     countInStock: 15,
   },
   {
+    id:2,
     name: "Iphone 12",
     imageUrl:
       "https://images.unsplash.com/photo-1605787020600-b9ebd5df1d07?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1463&q=80",
@@ -18,6 +20,7 @@ const products = [
     countInStock: 10,
   },
   {
+    id:3,
     name: "Cannon EOS-1D",
     imageUrl:
       "https://images.unsplash.com/photo-1519183071298-a2962feb14f4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
@@ -27,6 +30,7 @@ const products = [
     countInStock: 5,
   },
   {
+    id:4,
     name: "Amazon Alexa",
     imageUrl:
       "https://images.unsplash.com/photo-1518444065439-e933c06ce9cd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1267&q=80",
@@ -36,6 +40,7 @@ const products = [
     countInStock: 25,
   },
   {
+    id:5
     name: "Audio Technica Headphones",
     imageUrl:
       "https://images.unsplash.com/photo-1558756520-22cfe5d382ca?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
@@ -44,7 +49,7 @@ const products = [
     price: 233,
     countInStock: 4,
   },
-  {
+  {id:6,
     name: "JBL FLIP 4",
     imageUrl:
       "https://images.unsplash.com/photo-1564424224827-cd24b8915874?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1489&q=80",
