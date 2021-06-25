6b10255c48fec432581bbb84b30a7dfe570c1dce
omidiora <omidioraemmanuel@gmail.com>
2021-06-25 17:28:36 +0100

    fix add

--- frontend/src/components/data/products.js
+++ frontend/src/components/data/products.js
@@ -40,7 +40,7 @@ const products = [
     countInStock: 25,
   },
   {
-    id:5
+    id:5,
     name: "Audio Technica Headphones",
     imageUrl:
       "https://images.unsplash.com/photo-1558756520-22cfe5d382ca?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
